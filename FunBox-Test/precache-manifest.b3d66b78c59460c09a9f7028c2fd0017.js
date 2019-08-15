780c6324ceb5fed4d4a9db2d989c62e996cb0261
arkhannanov <arkhannanov@gmail.com>
2019-08-15 22:46:01 +0300

    Build

---
+++ FunBox-Test/precache-manifest.b3d66b78c59460c09a9f7028c2fd0017.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc95251e26082d1e6237c92c1f42c2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16b399880170bd1d19ba",
+    "url": "/static/css/main.fb6ab4bd.chunk.css"
+  },
+  {
+    "revision": "7382c095cef2f51904ff",
+    "url": "/static/js/2.277fa4c4.chunk.js"
+  },
+  {
+    "revision": "16b399880170bd1d19ba",
+    "url": "/static/js/main.05c788c6.chunk.js"
+  },
+  {
+    "revision": "d2023f07f34526a654ad",
+    "url": "/static/js/runtime~main.296729df.js"
+  }
+]);
